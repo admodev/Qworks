3cc771266177dff36c4a0e9536ca71bce536ba29
admodev <admosdigital@yahoo.com>
2020-10-06 16:45:55 -0300

    agregar bottomNavigator

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import "react-native-gesture-handler";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { AppRegistry, StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { BottomNavigation, Text } from "react-native-paper";
 import OnboardingPage from "./pages/OnboardingPage";
 import LoginPage from "./pages/LoginPage";
 import ChatPage from "./pages/ChatPage";
@@ -14,80 +14,115 @@ import SearchPage from "./pages/SearchPage";
 import ProfilePage from "./pages/ProfilePage";
 import AnunciatePage from "./pages/AnunciatePage";
 import PagosPage from "./pages/PagosPage";
-import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID } from "@env";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+} from "@env";
 import * as firebase from "firebase";
 
 const Stack = createStackNavigator();
-const Tab = createMaterialTopTabNavigator();
 
 function App() {
+  function TabsNav() {
+    const MusicRoute = () => <Text>Music</Text>;
+
+    const AlbumsRoute = () => <Text>Albums</Text>;
+
+    const RecentsRoute = () => <Text>Recents</Text>;
+
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+      { key: "music", title: "Music", icon: "queue-music" },
+      { key: "albums", title: "Albums", icon: "album" },
+      { key: "recents", title: "Recents", icon: "history" },
+    ]);
+
+    const renderScene = BottomNavigation.SceneMap({
+      music: MusicRoute,
+      albums: AlbumsRoute,
+      recents: RecentsRoute,
+    });
     return (
-        <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName="ProfilePage"
-                screenOptions={{
-                    headerStyle: { backgroundColor: "#633689" },
-                    headerTintColor: "#fff",
-                    headerTitleStyle: { fontWeight: "bold" },
-                }}
-            >
-                <Stack.Screen
-                    name="OnboardingPage"
-                    component={OnboardingPage}
-                    options={{ title: "QueDeOficios!", headerShown: false }}
-                />
-                <Stack.Screen
-                    name="SearchPage"
-                    component={SearchPage}
-                    options={{ title: "QuedeOficios!", headerShown: false }}
-                />
-                <Stack.Screen
-                    name="ChatPage"
-                    component={ChatPage}
-                    options={{ title: "QuedeOficios!", headerShown: false }}
-                />
-                <Stack.Screen
-                    name="RegisterPage"
-                    component={RegisterPage}
-                    options={{ title: "QuedeOficios!", headerShown: false }}
-                />
-                <Stack.Screen
-                    name="EmailLoginPage"
-                    component={EmailLoginPage}
-                    options={{ title: "QuedeOficios!", headerShown: false }}
-                />
-                <Stack.Screen
-                    name="EmailRegisterPage"
-                    component={EmailRegisterPage}
-                    options={{ title: "QuedeOficios!", headerShown: false }}
-                />
-                <Stack.Screen
-                    name="ProfilePage"
-                    component={ProfilePage}
-                    options={{ title: "QuedeOficios!", headerShown: false }}
-                />
-                <Stack.Screen
-                    name="AnunciatePage"
-                    component={AnunciatePage}
-                    options={{ title: "QuedeOficios!", headerShown: false }}
-                />
-                <Stack.Screen
-                    name="PagosPage"
-                    component={PagosPage}
-                    options={{ title: "QuedeOficios!", headerShown: false }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+      />
     );
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="OnboardingPage"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#633689" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      >
+        <Stack.Screen
+          name="OnboardingPage"
+          component={OnboardingPage}
+          options={{ title: "QueDeOficios!", headerShown: false }}
+        />
+        <Stack.Screen
+          name="SearchPage"
+          component={SearchPage}
+          options={{ title: "QuedeOficios!", headerShown: false }}
+        />
+        <Stack.Screen
+          name="ChatPage"
+          component={ChatPage}
+          options={{ title: "QuedeOficios!", headerShown: false }}
+        />
+        <Stack.Screen
+          name="RegisterPage"
+          component={RegisterPage}
+          options={{ title: "QuedeOficios!", headerShown: false }}
+        />
+        <Stack.Screen
+          name="EmailLoginPage"
+          component={EmailLoginPage}
+          options={{ title: "QuedeOficios!", headerShown: false }}
+        />
+        <Stack.Screen
+          name="EmailRegisterPage"
+          component={EmailRegisterPage}
+          options={{ title: "QuedeOficios!", headerShown: false }}
+        />
+        <Stack.Screen
+          name="ProfilePage"
+          component={ProfilePage}
+          options={{ title: "QuedeOficios!", headerShown: false }}
+        />
+        <Stack.Screen
+          name="AnunciatePage"
+          component={AnunciatePage}
+          options={{ title: "QuedeOficios!", headerShown: false }}
+        />
+        <Stack.Screen
+          name="PagosPage"
+          component={PagosPage}
+          options={{ title: "QuedeOficios!", headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default App;
+
+AppRegistry.registerComponent("main", () => "App");
--- pages/OnboardingPage.js
+++ pages/OnboardingPage.js
@@ -1,243 +1,286 @@
-import React from "react";
+import * as React from "react";
 import {
-    TouchableOpacity,
-    StyleSheet,
-    Image,
-    View,
-    ScrollView,
-    Text,
-    SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  View,
+  ScrollView,
+  SafeAreaView,
 } from "react-native";
 import { Button, Card, Icon, Input } from "react-native-elements";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
+import { BottomNavigation, Text } from "react-native-paper";
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
 
 const OnboardingPage = ({ navigation }) => {
-    return (
-        <SafeAreaView style={{ flex: 1, position: "absolute" }}>
-            <Text style={{ color: "#000000"}}>Hola!</Text>
-        </SafeAreaView>
-            <SafeAreaView style={{ flex: 1, backgroundColor: "beige" }}>
-            <ScrollView showsVerticalScrollIndicator={false} style={{ position: "relative", marginTop: 103 }}>
-                <Card containerStyle={styles.primeraCard}>
-                    <Image
-                        source={require("../assets/patron.jpg")}
-                        style={{
-                            flex: 1,
-                            position: "absolute",
-                            resizeMode: "cover",
-                            width: "100%",
-                            height: "100%",
-                        }}
-                    />
-                    <Image
-                        source={require("../assets/gradients/20x20.png")}
-                        style={{
-                            flex: 1,
-                            position: "absolute",
-                            resizeMode: "cover",
-                            width: "100%",
-                            height: "100%",
-                            opacity: 0.9
-                        }}
-                    />
-                    <Card.Image
-                        source={{
-                            uri:
-                            "https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ",
-                        }}
-                        style={{ borderRadius: 50, marginTop: 10, marginBottom: 20, marginLeft: 60, marginRight: 60 }}
-                    />
-                    <Text
-                        style={{
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            fontSize: 20,
-                            color: "#fff",
-                        }}
-                    >
-                        Alejandro Rodriguez
-                    </Text>
-                    <Text
-                        style={{
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            marginBottom: 10,
-                            fontSize: 20,
-                            color: "#fff",
-                        }}
-                    >
-                        Redactor
-                    </Text>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate("ChatPage")}
-                        style={{
-                            borderRadius: 0,
-                            marginLeft: 0,
-                            marginRight: 0,
-                            marginBottom: 0,
-                            backgroundColor: "transparent",
-                        }}
-                    >
-                        <Text
-                            style={{ color: "#fff", marginLeft: "auto", marginRight: "auto" }}
-                        >
-                            Contratar
-                        </Text>
-                    </TouchableOpacity>
-                </Card>
-                <Card containerStyle={styles.card}>
-                    <Card.Image
-                        source={{
-                            uri:
-                            "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk",
-                        }}
-                        style={{ borderRadius: 50, margin: 30 }}
-                    />
-                    <Text
-                        style={{
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            marginBottom: 10,
-                            fontSize: 20,
-                            color: "#fff",
-                        }}
-                    >
-                        Luciana Gomez
-                    </Text>
-                    <Text
-                        style={{
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            marginBottom: 10,
-                            fontSize: 20,
-                            color: "#fff",
-                        }}
-                    >
-                        Diseñadora Gráfica
-                    </Text>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate("ChatPage")}
-                        style={{
-                            borderRadius: 0,
-                            marginLeft: 0,
-                            marginRight: 0,
-                            marginBottom: 0,
-                            backgroundColor: "transparent",
-                        }}
-                    >
-                        <Text
-                            style={{ color: "#fff", marginLeft: "auto", marginRight: "auto" }}
-                        >
-                            Contratar
-                        </Text>
-                    </TouchableOpacity>
-                </Card>
-                <Card containerStyle={styles.card}>
-                    <Card.Image
-                        source={{
-                            uri:
-                            "https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY",
-                        }}
-                        style={{ borderRadius: 50, margin: 30 }}
-                    />
-                    <Text
-                        style={{
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            marginBottom: 10,
-                            fontSize: 20,
-                            color: "#fff",
-                        }}
-                    >
-                        Martín Alvaro
-                    </Text>
-                    <Text
-                        style={{
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            marginBottom: 10,
-                            fontSize: 20,
-                            color: "#fff",
-                        }}
-                    >
-                        Decoración de Interiores
-                    </Text>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate("ChatPage")}
-                        style={{
-                            borderRadius: 0,
-                            marginLeft: 0,
-                            marginRight: 0,
-                            marginBottom: 0,
-                            backgroundColor: "transparent",
-                        }}
-                    >
-                        <Text
-                            style={{ color: "#fff", marginLeft: "auto", marginRight: "auto" }}
-                        >
-                            Contratar
-                        </Text>
-                    </TouchableOpacity>
-                </Card>
-            </ScrollView>
-            <View style={{ flex: 1, position: "absolute", alignContent: "center", justifyContent: "center", marginTop: 25 }}>
-                <TouchableOpacity style={{ flex: 1, flexDirection: "row", marginTop: 30 }}>
-                    <Input placeholder="Buscar en  ¡QuedeOficios!" leftIcon={
-                        <Image
-                            source={require("../assets/icon.png")}
-                            style={{
-                                width: 35,
-                                height: 35,
-                            }}
-                        />
-                        }
-                        inputStyle={{ marginLeft: 10 }}
-                    />
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    );
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "music", title: "Music", icon: "queue-music" },
+    { key: "albums", title: "Albums", icon: "album" },
+    { key: "recents", title: "Recents", icon: "history" },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    music: MusicRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+  });
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "beige" }}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{ position: "relative", marginTop: 103 }}
+      >
+        <Card containerStyle={styles.primeraCard}>
+          <Image
+            source={require("../assets/patron.jpg")}
+            style={{
+              flex: 1,
+              position: "absolute",
+              resizeMode: "cover",
+              width: "100%",
+              height: "100%",
+            }}
+          />
+          <Image
+            source={require("../assets/gradients/20x20.png")}
+            style={{
+              flex: 1,
+              position: "absolute",
+              resizeMode: "cover",
+              width: "100%",
+              height: "100%",
+              opacity: 0.9,
+            }}
+          />
+          <Card.Image
+            source={{
+              uri:
+                "https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ",
+            }}
+            style={{
+              borderRadius: 50,
+              marginTop: 10,
+              marginBottom: 20,
+              marginLeft: 60,
+              marginRight: 60,
+            }}
+          />
+          <Text
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              fontSize: 20,
+              color: "#fff",
+            }}
+          >
+            Alejandro Rodriguez
+          </Text>
+          <Text
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom: 10,
+              fontSize: 20,
+              color: "#fff",
+            }}
+          >
+            Redactor
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ChatPage")}
+            style={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0,
+              backgroundColor: "transparent",
+            }}
+          >
+            <Text
+              style={{ color: "#fff", marginLeft: "auto", marginRight: "auto" }}
+            >
+              Contratar
+            </Text>
+          </TouchableOpacity>
+        </Card>
+        <Card containerStyle={styles.card}>
+          <Card.Image
+            source={{
+              uri:
+                "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk",
+            }}
+            style={{ borderRadius: 50, margin: 30 }}
+          />
+          <Text
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom: 10,
+              fontSize: 20,
+              color: "#fff",
+            }}
+          >
+            Luciana Gomez
+          </Text>
+          <Text
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom: 10,
+              fontSize: 20,
+              color: "#fff",
+            }}
+          >
+            Diseñadora Gráfica
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ChatPage")}
+            style={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0,
+              backgroundColor: "transparent",
+            }}
+          >
+            <Text
+              style={{ color: "#fff", marginLeft: "auto", marginRight: "auto" }}
+            >
+              Contratar
+            </Text>
+          </TouchableOpacity>
+        </Card>
+        <Card containerStyle={styles.card}>
+          <Card.Image
+            source={{
+              uri:
+                "https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY",
+            }}
+            style={{ borderRadius: 50, margin: 30 }}
+          />
+          <Text
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom: 10,
+              fontSize: 20,
+              color: "#fff",
+            }}
+          >
+            Martín Alvaro
+          </Text>
+          <Text
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom: 10,
+              fontSize: 20,
+              color: "#fff",
+            }}
+          >
+            Decoración de Interiores
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ChatPage")}
+            style={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0,
+              backgroundColor: "transparent",
+            }}
+          >
+            <Text
+              style={{ color: "#fff", marginLeft: "auto", marginRight: "auto" }}
+            >
+              Contratar
+            </Text>
+          </TouchableOpacity>
+        </Card>
+      </ScrollView>
+      <View>
+        <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          position: "absolute",
+          alignContent: "center",
+          justifyContent: "center",
+          marginTop: 25,
+        }}
+      >
+        <TouchableOpacity
+          style={{ flex: 1, flexDirection: "row", marginTop: 30 }}
+        >
+          <Input
+            placeholder="Buscar en  ¡QuedeOficios!"
+            leftIcon={
+              <Image
+                source={require("../assets/icon.png")}
+                style={{
+                  width: 35,
+                  height: 35,
+                }}
+              />
+            }
+            inputStyle={{ marginLeft: 10 }}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    button: {
-        alignItems: "center",
-        backgroundColor: "#DDDDDD",
-        padding: 10,
-        width: 300,
-        marginTop: 16,
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    width: 300,
+    marginTop: 16,
+  },
+  primeraCard: {
+    marginTop: 50,
+    backgroundColor: "#483D8B",
+    shadowColor: "#000",
+    borderRadius: 15,
+    paddingTop: -5,
+    paddingBottom: 2,
+    marginBottom: 100,
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    primeraCard: {
-        marginTop: 50,
-        backgroundColor: "#483D8B",
-        shadowColor: "#000",
-        borderRadius: 15,
-        paddingTop: -5,
-        paddingBottom: 2,
-        marginBottom: 100,
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
+  },
+  card: {
+    backgroundColor: "#483D8B",
+    shadowColor: "#000",
+    borderRadius: 15,
+    paddingTop: -5,
+    paddingBottom: 2,
+    marginBottom: 100,
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    card: {
-        backgroundColor: "#483D8B",
-        shadowColor: "#000",
-        borderRadius: 15,
-        paddingTop: -5,
-        paddingBottom: 2,
-        marginBottom: 100,
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-    },
+  },
 });
 
 export default OnboardingPage;
