d42ff091801f3e46cfbd821e07d0010d9f476e9f
admodev <admosdigital@yahoo.com>
2020-12-29 15:22:46 -0300

    fix cambiar nombre

--- components/CambiarNombreComponent.js
+++ components/CambiarNombreComponent.js
@@ -1,46 +1,55 @@
-import React, { useState, setState } from "react";
-import { SafeAreaView } from "react-native";
-import { Button, Input } from "react-native-elements";
-import * as firebase from "firebase";
-import "firebase/database";
-import "firebase/auth";
+import React, { useState, setState } from 'react';
+import { SafeAreaView } from 'react-native';
+import { Button, Input } from 'react-native-elements';
+import * as firebase from 'firebase';
+import 'firebase/database';
+import 'firebase/auth';
+import * as Updates from 'expo-updates';
 
 export default function CambiarNombreScreen() {
-  let nombreNuevo = useState("");
+  let [newName, setNewName] = useState('');
   let user = firebase.auth().currentUser;
   let userId = user.uid;
 
-  function updateName(nombreNuevo) {
-    firebase
-      .database()
-      .ref("anuncios/")
-      .orderByChild("id")
-      .equalTo(userId)
-      .once("value", function (snap) {
-        snap.ref.update({ nombre: nombreNuevo });
-      });
+  function updateName(newName) {
+    user
+      .updateProfile({
+        displayName: newName,
+      })
+      .then(
+        function () {
+          var displayName = user.displayName;
+          Updates.reloadAsync();
+        },
+        function (error) {
+          alert(
+            'Hubo un error al cambiar su nombre, por favor intentelo de nuevo...'
+          );
+        }
+      );
   }
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Input
-        placeholder="Escribe tu nombre..."
-        value={nombreNuevo}
-        onChangeText={(nombreNuevo) => setState(nombreNuevo)}
+        placeholder='Escribe tu nombre...'
+        onChangeText={(newName) => setNewName(newName)}
+        value={newName}
         inputContainerStyle={{
-          width: "85%",
-          marginLeft: "auto",
-          marginRight: "auto",
-          marginTop: "25%",
+          width: '85%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: '25%',
         }}
       />
       <Button
-        onPress={() => updateName()}
+        title='Cambiar Nombre'
+        onPress={() => updateName(newName)}
         buttonStyle={{
-          backgroundColor: "orange",
+          backgroundColor: 'orange',
           borderRadius: 25,
-          width: "70%",
-          alignSelf: "center",
+          width: '70%',
+          alignSelf: 'center',
           marginTop: 10,
         }}
       />
