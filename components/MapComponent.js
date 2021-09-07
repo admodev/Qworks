a6763df8c742c2297e90b95430cb980df2e3e845
admodev <adolfomoyano@exegesisba.com>
2021-09-07 15:52:14 -0300

    minor bugfixes

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ActivityIndicator,
   Dimensions,
@@ -10,20 +10,14 @@ import {
   SafeAreaView,
   Text,
   Platform,
-  Alert,
 } from 'react-native';
-import { Avatar, Button, Card, Icon, Input } from 'react-native-elements';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Input } from 'react-native-elements';
+import * as Location from 'expo-location';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 import 'firebase/database';
 import 'firebase/auth';
-import * as RootNavigation from '../RootNavigation.js';
-import { StackActions } from '@react-navigation/native';
-import SearchedCardResult from './searchedCard';
 import MapView, { Marker } from 'react-native-maps';
-import Carousel from 'react-native-snap-carousel';
-import FotoMapa from './FotoMapa';
 
 const { width, height } = Dimensions.get('window');
 const SCREEN_WIDTH = width;
@@ -32,30 +26,26 @@ const ASPECT_RATIO = width / height;
 const LATITUDE_DELTA = 1.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-var itm = [];
+var region = {};
 
-class MapComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      search: '',
-      ready: false,
-      where: { lat: null, lng: null },
-      error: null,
-      showsUserLocation: true,
-      followsUserLocation: true,
-    };
-  }
+const MapComponent = () => {
+  const [items, setItems] = useState([]);
+  const [search, setSearch] = useState('');
+  const [ready, setReady] = useState(false);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const showsUserLocation = true;
+  const followsUserLocation = true;
 
-  componentDidMount() {
+  useEffect(() => {
     firebase
       .database()
       .ref('anuncios/')
+      .orderByKey()
       .on('value', (snap) => {
-        let items = [];
+        let itemsArr = [];
         snap.forEach((child) => {
-          items.push({
+          itemsArr.push({
             anuncioId: child.val().anuncioId,
             nombre: child.val().nombre,
             apellido: child.val().apellido,
@@ -74,245 +64,170 @@ class MapComponent extends Component {
             recomendacionesTotales: child.val().recomendacionesTotales,
             latitud: child.val().latitud,
             longitud: child.val().longitud,
-            uuid: child.val().uuid,
           });
         });
-        itm = items;
-        this.setState({ items: items });
+        setItems(itemsArr);
       });
 
-    this.setState({ ready: false, error: null });
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
 
-    let geoOptions = {
-      enableHighAccuracy: true,
-      timeout: 20000,
-      maximumAge: 60 * 60 * 24,
-    };
-    navigator.geolocation.getCurrentPosition(
-      this.geoSuccess,
-      this.geoFailure,
-      geoOptions
-    );
-  }
-  geoSuccess = (position) => {
-    this.setState({
-      ready: true,
-      where: { lat: position.coords.latitude, lng: position.coords.longitude },
-    });
-  };
-  geoFailure = (error) => {
-    this.setState({ error: err.message });
-  };
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
-  filterList(items) {
-    return items.filter(
-      (itm) =>
-        itm.actividad.toLowerCase().includes(this.state.search.toLowerCase()) ||
-        itm.palabraClaveUno
-          .toLowerCase()
-          .includes(this.state.search.toLowerCase()) ||
-        itm.palabraClaveDos
-          .toLowerCase()
-          .includes(this.state.search.toLowerCase()) ||
-        itm.palabraClaveTres
-          .toLowerCase()
-          .includes(this.state.search.toLowerCase())
-    );
-  }
-
-  render() {
-    var latitud = parseFloat(this.state.where.lat);
-    var longitud = parseFloat(this.state.where.lng);
-
-    const region = {
-      latitude: latitud,
-      longitude: longitud,
+  let text = 'Cargando...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    region = {
+      latitude: parseFloat(location.coords.latitude),
+      longitude: parseFloat(location.coords.longitude),
       latitudeDelta: LATITUDE_DELTA,
       longitudeDelta: LONGITUDE_DELTA,
     };
+  }
 
-    const closeControlPanel = () => {
-      _drawer.close();
-    };
+  function filterList(items) {
+    return items.filter(
+      (itm) =>
+        itm.actividad.toLowerCase().includes(search.toLowerCase()) ||
+        itm.palabraClaveUno.toLowerCase().includes(search.toLowerCase()) ||
+        itm.palabraClaveDos.toLowerCase().includes(search.toLowerCase()) ||
+        itm.palabraClaveTres.toLowerCase().includes(search.toLowerCase())
+    );
+  }
 
-    return (
-      <SafeAreaView style={{ flex: 35 }}>
-        <View style={styles.container}>
-          {!this.state.ready && (
-            <View
+  return (
+    <SafeAreaView style={{ flex: 35 }}>
+      <View style={styles.container}>
+        {items.length <= 0 && (
+          <View
+            style={{
+              ...Platform.select({
+                android: {
+                  padding: 10,
+                  marginTop: '10%',
+                  ...StyleSheet.absoluteFillObject,
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                },
+                ios: {
+                  padding: 10,
+                  marginTop: '20%',
+                  ...StyleSheet.absoluteFillObject,
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                },
+              }),
+            }}>
+            <Text
+              style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 15 }}>
+              Cargando...
+            </Text>
+            <ActivityIndicator
+              size='large'
+              color='orange'
               style={{
                 ...Platform.select({
                   android: {
-                    padding: 10,
-                    marginTop: '10%',
-                    ...StyleSheet.absoluteFillObject,
-                    justifyContent: 'flex-end',
-                    alignItems: 'center',
-                  },
-                  ios: {
-                    padding: 10,
-                    marginTop: '20%',
-                    ...StyleSheet.absoluteFillObject,
-                    justifyContent: 'flex-end',
-                    alignItems: 'center',
+                    marginBottom: '50%',
                   },
                 }),
-              }}>
-              <Text
-                style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 15 }}>
-                Cargando...
-              </Text>
-              <ActivityIndicator
-                size='large'
-                color='orange'
-                style={{
-                  ...Platform.select({
-                    android: {
-                      marginBottom: '50%',
-                    },
-                  }),
-                }}
-              />
-            </View>
-          )}
-          {this.state.error && <Text>{this.state.error}</Text>}
-          {this.state.ready && (
-            <MapView
-              ref={(ref) => {
-                this.mapRef = ref;
               }}
-              provider='google'
-              style={styles.map}
-              scrollEnabled={true}
-              zoomEnabled={true}
-              pitchEnabled={true}
-              rotateEnabled={true}
-              initialRegion={region}
-              // onUserLocationChange={(event) => console.log(event.nativeEvent)}
-              showsUserLocation={this.state.showsUserLocation}
-              fitToElements={true}>
-              {this.state.search
-                ? this.filterList(this.state.items).map((itm, i) => (
+            />
+          </View>
+        )}
+        {items.length > 0 && (
+          <MapView
+            provider='google'
+            style={styles.map}
+            scrollEnabled={true}
+            zoomEnabled={true}
+            pitchEnabled={true}
+            rotateEnabled={true}
+            initialRegion={region}
+            // onUserLocationChange={(event) => console.log(event.nativeEvent)}
+            showsUserLocation={showsUserLocation}
+            fitToElements={true}>
+            {search
+              ? filterList(items).map((itm, i) => (
+                  <Marker
+                    coordinate={{
+                      latitude: itm.partidoLatitude,
+                      longitude: itm.partidoLongitude,
+                    }}
+                    pinColor={'#fd5d13'}
+                    title={itm.nombre}
+                    description={itm.actividad}
+                    key={Math.max(i) + 1}
+                    focusable={true}
+                  />
+                ))
+              : items.map((element, index) => {
+                  return (
                     <Marker
                       coordinate={{
-                        latitude: itm.partidoLatitude,
-                        longitude: itm.partidoLongitude,
+                        latitude: element.partidoLatitude,
+                        longitude: element.partidoLongitude,
                       }}
                       pinColor={'#fd5d13'}
-                      onCalloutPress={() =>
-                        firebase.auth().currentUser
-                          ? RootNavigation.navigate('ChatComponent', {
-                              userOneId: firebase.auth().currentUser.uid,
-                              userTwoId: itm.idAnuncio,
-                              uuid: itm.uuid,
-                            })
-                          : Alert.alert(
-                              'Alerta',
-                              'Por favor ingresa para entablar una conversacion!'
-                            )
-                      }
-                      onPress={() =>
-                        firebase.auth().currentUser
-                          ? RootNavigation.navigate('ChatComponent', {
-                              userOneId: firebase.auth().currentUser.uid,
-                              userTwoId: itm.idAnuncio,
-                              uuid: itm.uuid,
-                            })
-                          : Alert.alert(
-                              'Alerta',
-                              'Por favor ingresa para entablar una conversacion!'
-                            )
-                      }
-                      title={itm.nombre}
-                      description={itm.actividad}
-                      key={Math.max(i) + 1}
+                      title={element.nombre}
+                      description={element.actividad}
+                      key={Math.max(index) + 1}
                       focusable={true}
                     />
-                  ))
-                : this.state.items.map((element, index) => {
-                    return (
-                      <Marker
-                        coordinate={{
-                          latitude: element.partidoLatitude,
-                          longitude: element.partidoLongitude,
-                        }}
-                        onCalloutPress={() =>
-                          firebase.auth().currentUser
-                            ? RootNavigation.navigate('ChatComponent', {
-                                userOneId: firebase.auth().currentUser.uid,
-                                userTwoId: element.idAnuncio,
-                                uuid: element.uuid,
-                              })
-                            : Alert.alert(
-                                'Alerta',
-                                'Por favor ingresa para entablar una conversacion!'
-                              )
-                        }
-                        onPress={() =>
-                          firebase.auth().currentUser
-                            ? RootNavigation.navigate('ChatComponent', {
-                                userOneId: firebase.auth().currentUser.uid,
-                                userTwoId: element.idAnuncio,
-                                uuid: element.uuid,
-                              })
-                            : Alert.alert(
-                                'Alerta',
-                                'Por favor ingresa para entablar una conversacion!'
-                              )
-                        }
-                        pinColor={'#fd5d13'}
-                        title={element.nombre}
-                        description={element.actividad}
-                        key={Math.max(index) + 1}
-                        focusable={true}
-                      />
-                    );
-                  })}
-            </MapView>
-          )}
-          <Input
-            placeholder='Buscar en Q works!'
-            containerStyle={{
-              ...Platform.select({
-                android: {
-                  position: 'absolute',
-                  top: 50,
-                  left: 20,
-                  maxWidth: '90%',
-                  alignSelf: 'center',
-                  backgroundColor: '#ffffff',
-                  borderRadius: 50,
-                  height: 40,
-                },
-                ios: {
-                  position: 'absolute',
-                  top: 20,
-                  left: 20,
-                  maxWidth: '90%',
-                  alignSelf: 'center',
-                  backgroundColor: '#ffffff',
-                  borderRadius: 50,
-                  height: 40,
-                },
-              }),
-            }}
-            inputStyle={{
-              justifyContent: 'center',
-              marginLeft: '25%',
-            }}
-            inputContainerStyle={{
-              borderWidth: 0,
-              border: 0,
-              borderBottomWidth: 0,
-            }}
-            placeholderTextColor='#000000'
-            onChangeText={(search) => this.setState({ search })}
-          />
-        </View>
-      </SafeAreaView>
-    );
-  }
-}
+                  );
+                })}
+          </MapView>
+        )}
+        <Input
+          placeholder='Buscar en Q works!'
+          containerStyle={{
+            ...Platform.select({
+              android: {
+                position: 'absolute',
+                top: 50,
+                left: 20,
+                maxWidth: '90%',
+                alignSelf: 'center',
+                backgroundColor: '#ffffff',
+                borderRadius: 50,
+                height: 40,
+              },
+              ios: {
+                position: 'absolute',
+                top: 20,
+                left: 20,
+                maxWidth: '90%',
+                alignSelf: 'center',
+                backgroundColor: '#ffffff',
+                borderRadius: 50,
+                height: 40,
+              },
+            }),
+          }}
+          inputStyle={{
+            justifyContent: 'center',
+            marginLeft: '25%',
+          }}
+          inputContainerStyle={{
+            borderWidth: 0,
+            border: 0,
+            borderBottomWidth: 0,
+          }}
+          placeholderTextColor='#000000'
+          onChangeText={(search) => setSearch(search)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
